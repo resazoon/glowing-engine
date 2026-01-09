918b7f91a304efe99ef0ee0c5502220ba6095ebf
resazoon <shanqitsuyazi@gmail.com>
2026-01-09 16:19:02 +0900

    main.mm

---
+++ main.mm
@@ -0,0 +1,32 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface MyESPView : UIView
+@end
+
+@implementation MyESPView
+- (void)drawRect:(CGRect)rect {
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetRGBFillColor(context, 1.0, 0.0, 0.0, 1.0); // 動作確認用の赤い四角
+    CGContextFillRect(context, CGRectMake(100, 100, 100, 100)); 
+}
+@end
+
+__attribute__((constructor))
+static void init() {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        UIWindow *window = nil;
+        for (UIWindowScene* scene in [UIApplication sharedApplication].connectedScenes) {
+            if (scene.activationState == UISceneActivationStateForegroundActive) {
+                window = scene.windows.firstObject;
+                break;
+            }
+        }
+        if (window) {
+            MyESPView *esp = [[MyESPView alloc] initWithFrame:window.bounds];
+            esp.backgroundColor = [UIColor clearColor];
+            esp.userInteractionEnabled = NO;
+            [window addSubview:esp];
+        }
+    });
+}
